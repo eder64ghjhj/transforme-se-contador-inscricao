8fcbf2421eae9af56d5b4e44579541d3543668bd
eder64ghjhj <danielviera1222tt@gmail.com>
2026-08-20 02:08:18 -0300

    feat: adicionamos em aula uma estrutura de repeticao para idade

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 
- 
+
 
 let vagas = 10;
 let inscritos = 0;
@@ -13,4 +13,22 @@ botao.addEventListener('click',function () {
         inscritos++;
         contador.textContent = `${inscritos} inscritos`;
     }
-});
+}); 
+
+
+
+let idade = 20;
+
+if(isNaN(idade)) {
+    console.log("Nao é um numero valido");
+} else if(idade < 0 || idade > 100) {
+    console.log("Idade nao existe");
+} else if( idade < 8) {
+    console.log("Menor de 8 anos,nao pode se inscrever");
+} else if (idade <= 18) {
+    console.log("Pode se inscrever (Menor de idade")
+} else if (idade <=30) {
+    console.log("Maior de idade ,pode se inscrever")
+} else{
+    console.log("Nao pode se inscrever,maior de 30")
+}
